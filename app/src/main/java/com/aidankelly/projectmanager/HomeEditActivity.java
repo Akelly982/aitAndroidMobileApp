1341ea764aaa29634517d2ade9989e88aca70f09
Akelly <akelly982@hotmail.com>
2020-05-06 23:01:19 +1000

    Add home edit xml and activity

---
+++ app/src/main/java/com/aidankelly/projectmanager/HomeEditActivity.java
@@ -0,0 +1,14 @@
+package com.aidankelly.projectmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeEditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_edit);
+    }
+}
